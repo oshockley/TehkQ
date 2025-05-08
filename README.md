cbc92f64d3c41dd4204ee91de4dea0cb341d4202
O'Neal Shockley <shockleyoneal@gmail.com>
2025-05-08 07:21:47 -0700

    made changes to e2e testing

---
+++ README.md
@@ -0,0 +1,26 @@
+# TehkQ
+
+## User Story
+- AS AN aspiring developer
+I WANT to take a tech quiz
+SO THAT I can test my knowledge and improve my skills
+
+## Acceptance Criteria 
+- GIVEN I am taking a tech quiz
+WHEN I click the start button
+THEN the quiz starts and I am presented with a question
+WHEN I answer a question
+THEN I am presented with another question
+WHEN all questions are answered
+THEN the quiz is over
+WHEN the quiz is over
+THEN I can view my score
+WHEN the quiz is over
+THEN I can start a new quiz
+
+## Features
+- 10 Question Multiple Choice Quiz
+- A series of 4 options of answers
+- Progress to the next question once answer is clicked
+- Final Score is visible once all questions are answered 
+- You are given the option to retake the quiz 
--- cypress/e2e/quiz.cy.jsx
+++ cypress/e2e/quiz.cy.jsx
@@ -14,17 +14,29 @@ describe ("quiz componet test", () => {
         cy.get('.card h2').should('exist');
     });
 
-    it('should finish the quiz and show the final score', () => {
+    it('should end the end the quiz and show score after all questions are answered', () => {
         cy.findByRole('button', { name: 'Start Quiz' }).click();
-        cy.completeQuiz();
 
-        cy.get('h2').should('have.text', 'Quiz Completed');
-        cy.get('[data-cy="score"]').should('exist');
+        //Answer all 10 questions
+        for (let i = 0; i < 10; i++) {
+            cy.get('.btn-primary').should('have.length', 4);
+            cy.get('.btn-primary').first().click();
+        }
+
+        //Verify score is shown
+        cy.contains(/score/i).should('exist');
     });
 
-    it('should show the option to start new quiz after completing the current one', () => {
-        cy.findByRole('button', { name: 'Start Quiz' }).click();
-        cy.completeQuiz();
-        cy.findByRole('button', { name: /take a new quiz/i }).should('exist');
+    it('should allow restarting the quiz after it is over', () => {
+        cy.findByRole('button', { name: /start quiz/i }).click();
+
+        for (let i = 0; i < 10; i++) {
+            cy.get('.btn-primary').first().click();
+        }
+
+        cy.contains(/score/i).should('exist');
+
+        cy.findByRole('button', { name: /quiz/i }).click();
+        cy.get('.card h2').should('exist');
     });
 });
